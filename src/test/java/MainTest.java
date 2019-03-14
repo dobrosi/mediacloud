d4269ab009ce66dd36eaa90e0f1c87b4fed99372
dobrosi <dobrosi.andras@hmei.hu>
2019-03-14 15:56:38 +0100

    1st commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import com.github.dobrosi.mediacloud.Main;
+
+/**
+ * Runs a simple UPnP discovery procedure.
+ */
+public class MainTest {
+
+	@Test
+	public void mainTest() throws Exception {
+		Main.main(null);
+	}
+}
