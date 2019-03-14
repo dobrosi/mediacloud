9dd3bcffc41198d72aa1ceb91f2bce8172397f6b
dobrosi <dobrosi.andras@hmei.hu>
2019-03-14 16:25:53 +0100

    123

--- src/main/java/com/github/dobrosi/mediacloud/Main.java
+++ src/main/java/com/github/dobrosi/mediacloud/Main.java
@@ -3,8 +3,10 @@ package com.github.dobrosi.mediacloud;
 import org.fourthline.cling.UpnpService;
 import org.fourthline.cling.UpnpServiceImpl;
 import org.fourthline.cling.model.message.header.STAllHeader;
+import org.fourthline.cling.model.meta.Device;
 import org.fourthline.cling.model.meta.LocalDevice;
 import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.model.meta.Service;
 import org.fourthline.cling.registry.Registry;
 import org.fourthline.cling.registry.RegistryListener;
 
@@ -15,38 +17,48 @@ public class Main {
 		// UPnP discovery is asynchronous, we need a callback
 		RegistryListener listener = new RegistryListener() {
 
+			@Override
 			public void remoteDeviceDiscoveryStarted(final Registry registry, final RemoteDevice device) {
 				System.out.println("Discovery started: " + device.getDisplayString());
 			}
 
+			@Override
 			public void remoteDeviceDiscoveryFailed(final Registry registry, final RemoteDevice device, final Exception ex) {
 				System.out.println("Discovery failed: " + device.getDisplayString() + " => " + ex);
 			}
 
+			@Override
 			public void remoteDeviceAdded(final Registry registry, final RemoteDevice device) {
 				System.out.println("Remote device available: " + device.getDisplayString());
 			}
 
+			@Override
 			public void remoteDeviceUpdated(final Registry registry, final RemoteDevice device) {
 				System.out.println("Remote device updated: " + device.getDisplayString());
 			}
 
+			@Override
 			public void remoteDeviceRemoved(final Registry registry, final RemoteDevice device) {
 				System.out.println("Remote device removed: " + device.getDisplayString());
 			}
 
+			@Override
 			public void localDeviceAdded(final Registry registry, final LocalDevice device) {
 				System.out.println("Local device added: " + device.getDisplayString());
 			}
 
+			@Override
 			public void localDeviceRemoved(final Registry registry, final LocalDevice device) {
 				System.out.println("Local device removed: " + device.getDisplayString());
 			}
 
+			@Override
 			public void beforeShutdown(final Registry registry) {
 				System.out.println("Before shutdown, the registry has devices: " + registry.getDevices().size());
+				registry.getDevices().forEach(d -> logDevice(d));
 			}
 
+			@Override
 			public void afterShutdown() {
 				System.out.println("Shutdown of registry complete!");
 
@@ -67,5 +79,18 @@ public class Main {
 		// Release all resources and advertise BYEBYE to other UPnP devices
 		System.out.println("Stopping Cling...");
 		upnpService.shutdown();
+	}
+
+	protected static Object logDevice(final Device device) {
+		System.out.println("name: " + device.getDisplayString());
+		for (Service s : device.findServices()) {
+			logService(s);
+		}
+		return null;
+	}
+
+	private static void logService(final Service service) {
+		System.out.println("service: " + service);
+
 	}
 }
